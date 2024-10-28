73466bc53ac3aed62c677b00586e96d2a90db166
aglaehernandez <aglaehernandez@gmail.com>
2024-10-28 15:32:23 +0800

    Added API

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
+function displayRecipe(response) {
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.innerHTML = response.data.answer;
+
+  console.log(response.data.answer);
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
-  let timeElement = document.querySelector("#time");
-  timeElement.innerHTML = `Total time: 10min`;
+  let ingredientInput = document.querySelector("#ingredientInput");
+  let recipeTypeInput = document.querySelector("#recipeTypeInput");
 
-  let servingsElement = document.querySelector("#servings");
-  servingsElement.innerHTML = `Servings: 3`;
+  let apiKey = `f9a47eo48c7db6t0bcbfca0c176bd333`;
+  let prompt = `The user instructions are: Generate a ${recipeTypeInput.value} recipe using ${ingredientInput.value}`;
+  let context = `You are an experienced recipe writer. Your mission is to generate a recipe following the user instructions.
+  Write your answer using basic HTML only include the body part of HTML (but don't write body) and include the total time to prepare, the number of servings, the list of ingredients and the instructions`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  let ingredientsElement = document.querySelector("#ingredients");
-  ingredientsElement.innerHTML = `<strong>Ingredients:</strong>
-            <br />
-            Raw rice - ½ cup
-            <br />
-            Milk - 2-½ cups
-            <br />
-            Salt - to taste`;
+  axios.get(apiUrl).then(displayRecipe);
 
-            let instructionsElement = document.querySelector("#instructions");
-            instructionsElement.innerHTML = `<strong>Instructions:</strong>
-            <br />
-            1. boil pasta`;
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
 }
-
-
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
 recipeFormElement.addEventListener("submit", generateRecipe);
